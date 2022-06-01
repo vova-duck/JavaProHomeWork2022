3836741ae2ed21e994cb714eae8d50b9ac8397e9
Volodymyr Chryzhanovskyi <maslo_duck@icloud.com>
2022-06-01 22:22:58 +0300

    created Student class deleted default "hello world!" program

---
+++ src/StudentSystemID/Student.java
@@ -0,0 +1,13 @@
+package StudentSystemID;
+
+public class Student {
+    private String name;
+    private int id;
+    public Student(){
+
+    }
+    public Student(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+}
