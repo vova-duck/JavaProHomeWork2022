c2ab51404deec1404af56ff70429a7139e9428a3
Volodymyr Chryzhanovskyi <maslo_duck@icloud.com>
2022-06-01 22:25:20 +0300

    created Student class deleted default "hello world!" program created main class

---
+++ src/StudentSystemID/Main.java
@@ -0,0 +1,8 @@
+package StudentSystemID;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("Joseph", 2);
+        System.out.println(student);
+    }
+}
--- src/StudentSystemID/Student.java
+++ src/StudentSystemID/Student.java
@@ -10,4 +10,28 @@ public class Student {
         this.name = name;
         this.id = id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
 }
