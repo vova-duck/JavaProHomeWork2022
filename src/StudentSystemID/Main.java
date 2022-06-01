3e0d88cd5df607794ebfb7f1e6e352592d5492cc
vova-duck <90979748+vova-duck@users.noreply.github.com>
2022-06-02 00:27:29 +0300

    Update Main.java

--- src/StudentSystemID/Main.java
+++ src/StudentSystemID/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Student student = new Student("Joseph", 2);
         System.out.println(student);
+        System.out.println("Hello "+ student)
     }
 }
