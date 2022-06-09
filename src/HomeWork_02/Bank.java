6ffd3dfb26096a122a85f50f5148ebb2a0c5a781
Volodymyr Khryzhanovskyi <maslo_duck@icloud.com>
2022-06-09 17:00:36 +0300

    1.made back calculator, it's pretty accurate, but there's room to make it better.

--- src/HomeWork_02/Bank.java
+++ src/HomeWork_02/Bank.java
@@ -1,13 +1,25 @@
 package HomeWork_02;
 
+import Utils.Utils;
+
+import java.io.IOException;
+
 public class Bank {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("type how many money you put into deposit");
-        int deposit;
+        int deposit = Utils.giveInt();
         System.out.println("type how many years you'll keep that money on bank account");
-        int year;
+        int year = Utils.giveInt();
         System.out.println("type your percentage");
-        int percentage;
+        float percentage  = Utils.giveInt() * 0.01f;
+        int finalDeposit;
+        for (int i = 1; i < year +1; i++) {
+            finalDeposit = (int) (deposit * Math.pow((1+ percentage/12),12));
+            deposit = finalDeposit;
+            System.out.println("After "+ i +" years your account is " + deposit);
+        }
+        System.out.println("---------------");
+        System.out.println(deposit + " your final deposit");
     }
 
 }
