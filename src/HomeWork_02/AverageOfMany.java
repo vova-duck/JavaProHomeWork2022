8751bb471a6cdf6cdb576276012d3e02fc083a8a
Volodymyr Khryzhanovskyi <maslo_duck@icloud.com>
2022-06-09 23:30:55 +0300

    1.fixed bank calculator. Not it shows the as accurate as it can 2. Recoded Average calculator. now Its asking numbers constantly without asking how many numbers expected. But if you write down empty String witch is "stop word" it shows message that you have to put only numerous value.

--- src/HomeWork_02/AverageOfMany.java
+++ src/HomeWork_02/AverageOfMany.java
@@ -4,28 +4,23 @@ package HomeWork_02;
 import Utils.Utils;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
+//2. Программа, которая находит среднее арифметическое значение произвольного количества чисел.
 
 public class AverageOfMany {
     public static void main(String[] args) throws IOException {
-        System.out.println("type how many numbers you need to get average number from");
-        int numberCount = Utils.giveInt();
-        int[]numbers = new int[numberCount];
-        int consumption = 0;
-        for (int i = 0; i < numberCount; i++) {
-            System.out.println("type  number" );
-           try {
-               numbers[i]=Utils.giveInt();
-           }catch (NumberFormatException ttr){
-               System.out.println("you did mistake put number");
-               numbers[i]=Utils.giveInt();
-           }
+        int sum = 0;
+        int count = 0;
+        String input = "line";
+        while (!input.equals("")){
+            System.out.println("type numbers to add or push ENTER to calculate");
+            try{sum+=Integer.parseInt(input = Utils.giveString());
+                count++;
+            }
+            catch (NumberFormatException wft){
+                System.out.println("type number instead String:)");}
         }
-        for (int i = 0; i < numberCount; i++) {
-            consumption+=numbers[i];
-        }
-        System.out.println("average is " + consumption/numberCount);
-
-    }}
+        System.out.println( sum/count + " is your average");
+    }
+}
 
--- src/HomeWork_02/Bank.java
+++ src/HomeWork_02/Bank.java
@@ -1,4 +1,8 @@
 package HomeWork_02;
+//3. Программу, которая предлагает пользователю ввести сумму денежного вклада в гривнах, процент годовых,
+// которые выплачивает банк, длительность вклада (лет).
+// Вывести накопленную сумму денег за каждый год и начисленные проценты.
+// (проценты сложные с капитализацией каждый месяц)
 
 import Utils.Utils;
 
@@ -7,15 +11,16 @@ import java.io.IOException;
 public class Bank {
     public static void main(String[] args) throws IOException {
         System.out.println("type how many money you put into deposit");
-        int deposit = Utils.giveInt();
+        double deposit = Utils.giveInt();
         System.out.println("type how many years you'll keep that money on bank account");
-        int year = Utils.giveInt();
+        int depositYears = Utils.giveInt();
+        int monthsInTheYear = 12;
         System.out.println("type your percentage");
         float percentage  = Utils.giveInt() * 0.01f;
-        int finalDeposit;
-        for (int i = 1; i < year +1; i++) {
-            finalDeposit = (int) (deposit * Math.pow((1+ percentage/12),12));
-            deposit = finalDeposit;
+        double finalDeposit;
+        for (int i = 1; i < depositYears +1; i++) {
+            finalDeposit =  (deposit * Math.pow((1+ percentage/monthsInTheYear),monthsInTheYear));
+            deposit =  finalDeposit;
             System.out.println("After "+ i +" years your account is " + deposit);
         }
         System.out.println("---------------");
