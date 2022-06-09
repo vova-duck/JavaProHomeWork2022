f129feb884bb34a262bacfea9b4d81219650f6e5
Volodymyr Khryzhanovskyi <maslo_duck@icloud.com>
2022-06-09 12:32:30 +0300

    1.added variables and SOUTs)

--- src/HomeWork_02/Bank.java
+++ src/HomeWork_02/Bank.java
@@ -1,5 +1,13 @@
 package HomeWork_02;
 
 public class Bank {
+    public static void main(String[] args) {
+        System.out.println("type how many money you put into deposit");
+        int deposit;
+        System.out.println("type how many years you'll keep that money on bank account");
+        int year;
+        System.out.println("type your percentage");
+        int percentage;
+    }
 
 }
